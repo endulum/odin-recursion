e14233d97d2b06db812d6b81f192e4a6ee5eec11
endulum <apheum@zoho.com>
2023-03-07 12:38:56 -0800

    slicing array in half step

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let contents = [];
 
-while (contents.length < 6) {
+while (contents.length < 8) {
     contents.push(Math.floor(Math.random()*10));
 }
 
@@ -13,4 +13,16 @@ const merge = arr => {
         // sort left half of array
         // sort right half of array
         // merge these two halves
+    if (arr.length < 2) return
+    else {
+        let firstHalf = arr.slice(0, (arr.length / 2));
+        console.log(firstHalf);
+        let secondHalf = arr.slice((arr.length / 2), (arr.length));
+        console.log(secondHalf);
+        return [...firstHalf, ...secondHalf];
+        // merge(arr.slice(0, (arr.length / 2)));
+        // merge(arr.slice((arr.length / 2), (arr.length / 2)));
+    }
 }
+
+console.log(merge(contents));
