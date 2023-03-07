0e6e7b23081343f93c4e24e7e114dfb0d309ead2
endulum <apheum@zoho.com>
2023-03-07 12:28:34 -0800

    boilerplate, sample input, and pseudocode

---
+++ script.js
@@ -0,0 +1,16 @@
+let contents = [];
+
+while (contents.length < 6) {
+    contents.push(Math.floor(Math.random()*10));
+}
+
+console.log(contents);
+
+const merge = arr => {
+    // if length of array is less than 2
+        // return
+    // else
+        // sort left half of array
+        // sort right half of array
+        // merge these two halves
+}
