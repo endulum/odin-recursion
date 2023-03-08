f5c94e38bd86f58db02b70f817f8b9d6951462f1
endulum <apheum@zoho.com>
2023-03-07 19:42:38 -0800

    finally solved! but is there a better way...?

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 let contents = [];
 
-while (contents.length < 8) {
+while (contents.length < 12) {
     contents.push(Math.floor(Math.random()*10));
 }
 
 console.log(contents);
 
 const merge = arr => {
-    // if length of array is less than 2
-        // return
-    // else
-        // sort left half of array
-        // sort right half of array
-        // merge these two halves
-    if (arr.length < 2) return
+    if (arr.length < 2) return;
     else {
         let firstHalf = arr.slice(0, (arr.length / 2));
-        console.log(firstHalf);
         let secondHalf = arr.slice((arr.length / 2), (arr.length));
-        console.log(secondHalf);
-        return [...firstHalf, ...secondHalf];
-        // merge(arr.slice(0, (arr.length / 2)));
-        // merge(arr.slice((arr.length / 2), (arr.length / 2)));
+
+        merge(firstHalf);
+        merge(secondHalf);
+
+        let a = 0;
+        let b = 0;
+        let c = 0;
+
+        while (a < firstHalf.length && b < secondHalf.length) {
+            if (firstHalf[a] < secondHalf[b]) {
+                arr[c] = firstHalf[a];
+                a++;
+                if (a == firstHalf.length) {
+                    while (b < secondHalf.length) {
+                        c++;
+                        arr[c] = secondHalf[b];
+                        b++;
+                    }
+                }
+            } else {
+                arr[c] = secondHalf[b];
+                b++;
+                if (b == secondHalf.length) {
+                    while (a < firstHalf.length) {
+                        c++;
+                        arr[c] = firstHalf[a];
+                        a++;
+                    }
+                }
+            }
+            c++;
+        }
+
+        return arr;
     }
 }
 
